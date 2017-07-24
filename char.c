762cb29e47d70cefce9a11c9eb11c9d196cf7a56
Thirumalaikrishnanerode <30409064+Thirumalaikrishnanerode@users.noreply.github.com>
2017-07-24 18:22:41 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ 
+char c;
+printf("enter the character:");
+scanf("%c",&c);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+{
+printf("it is vowel");
+}
+else
+{
+printf("it is consonant");
+}
+return 0;
+}
